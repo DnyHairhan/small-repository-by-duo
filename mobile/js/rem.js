678388009408a7f8cb29303ba5e0e01837179f38
DnyHairhan <h.duoniya@icloud.com>
2017-04-11 15:05:23 +0800

    手机端简单布局

--- mobile/js/rem.js
+++ mobile/js/rem.js
@@ -6,7 +6,7 @@
 	var html =  document.documentElement;
     var hWidth = html.getBoundingClientRect().width;
     // 把页面的宽分成15分，相当于1rem = hWidth/15 + 'px';这个值不是固定值可以变
-    html.style.fontSize = hWidth/15 + 'px'; 
+    html.style.fontSize = hWidth/15 + 'px';         
     // console.log(hWidth);
     // console.log(html.style.fontSize);
 	
